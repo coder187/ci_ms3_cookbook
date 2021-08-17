5a1e66ef6a786c599dea9802fc36349cea5e4bfe
coder187 <coder187@hotmail.com>
2021-08-17 14:26:39 +0000

    add base and recipes page. Test connection to mongodb

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+from flask import (Flask, flash, render_template, redirect,
+                    request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
+
+# create an instance of Flask and set it to the variable app
+app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBANME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+# We need to setup an instance of PyMongo, 
+# and add the app into that using something called a constructor method, so type:
+mongo = PyMongo(app)
+
+
+@app.route("/")
+@app.route("/recipes")
+def recipes():
+    recipes = list(mongo.db.recipes.find())
+
+    return render_template("recipes.html", recipes=recipes)
+
+# set host and ip from env.py
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
